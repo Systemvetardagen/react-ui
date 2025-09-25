d3e26c1e1b62d8730426ef834a43c1e9c3f38d3f
Fredrik Etsare <Fredrik@etsare.se>
2025-09-25 22:59:34 +0200

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { peerDependencies } from "./package.json";
-import dts from "vite-plugin-dts";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    dts({ exclude: ["**/*.stories.tsx", "**/*.test.ts"] }),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     lib: {
       entry: "./src/index.ts",
